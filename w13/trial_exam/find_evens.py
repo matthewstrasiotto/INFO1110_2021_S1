6665fa3e63fb06d2a340f0c396b4edb263996d27
Matthew Strasiotto <39424834+matthewstrasiotto@users.noreply.github.com>
2021-06-01 10:31:28 +1000

    find evens

--- w13/trial_exam/find_evens.py
+++ w13/trial_exam/find_evens.py
@@ -1,2 +1,10 @@
-def find_evens(   ):
-    pass
+def find_evens(start, end):
+    out = ""
+    for i in range(start, end + 1):
+        if i % 2 == 0: out += f"{i} "
+
+    out = out.rstrip()
+
+    return out
+    
+print(find_evens(2, 10))
